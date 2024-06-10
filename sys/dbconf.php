f48ba86d1ce5859da1db33c3b12d0a10195aad29
L. Kirstein <leon.kirstein@lkirstein.ch>
2024-06-10 18:28:13 +0200

    Create dbconf.php
    dbconf.php is the database connection script which stores all connection details (for now) and provides other scripts with said database connection.

---
+++ sys/dbconf.php
@@ -0,0 +1,21 @@
+<?php
+
+  // Database Connection Details
+  // Will be set by the installation script.
+  // Change to fit your Development-Environment
+  $Servername = "localhost";
+  $Username = "root";
+  $Password = "root";
+  $Database = "lcms";
+
+  // Create connection
+  $conn = new mysqli($Servername, $Username, $Password, $Database);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  echo "Successfully connected to Database!";
+
+?> 
