743db19727846eb61849197780717ec16d91b6a8
L. Kirstein <leon.kirstein@lkirstein.ch>
2024-06-11 10:42:34 +0200

    Added function to set timestamp
    Added a function that sets a new timestamp for the session.

--- sesconf.php
+++ sesconf.php
@@ -3,4 +3,11 @@
     // Sessins is Calculated Minutes * Seconds
     $Session_Duration = (5 * 60);
 
+    function setNewTimeStamp() {
+
+        // Set new time for session
+        $_SESSION["timestamp"] = time();
+        
+    }
+
 ?>
