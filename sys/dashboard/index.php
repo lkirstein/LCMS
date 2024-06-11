6cc6f46ddeac6ebe911663faff9e286b8e9ec00c
L. Kirstein <leon.kirstein@lkirstein.ch>
2024-06-11 17:15:18 +0200

    Add timestamp check
    Added timestamp check to expire session after set duration (duration is set in sesconf.php). Renamed session variables.

--- sys/dashboard/index.php
+++ sys/dashboard/index.php
@@ -1,14 +1,27 @@
 <?php
 
+include ($_SERVER["DOCUMENT_ROOT"]."/sys/sesconf.php");
+
 // Initialize the session
 session_start();
  
-// Check if the user is logged in, if not then redirect him to login page
-if( !isset( $_SESSION["loggedin"] ) || $_SESSION["loggedin"] !== true)
+// Check if the "active" variable exists or if it isn't true
+if( !isset( $_SESSION["active"] ) || $_SESSION["active"] !== true)
 {
-    header("location: login.php");
+    // Redirect to login page
+    doRedirect("./../login.php");
     exit;
 }
+// Session exists
+else 
+{
+    if ( !checkSessionTimestamp($Session_Duration) )
+    {
+        destroySession();
+    }
+}
+
+print_r($_SESSION);
 
 ?>
 
