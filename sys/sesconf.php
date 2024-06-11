b23aa4c6998d8b1373b739091ee2ba89b441f97f
L. Kirstein <leon.kirstein@lkirstein.ch>
2024-06-11 17:11:05 +0200

    Added session management functions
    Added more session management functions such as "destroySession", "doRedirect", "setSessionTimestamp" an "checkSessionTimestamp".

---
+++ sys/sesconf.php
@@ -0,0 +1,45 @@
+<?php
+
+    // Sessins is Calculated Minutes * Seconds
+    $Session_Duration = (120 * 60);
+
+    function destroySession() {
+        // Destroy session.
+        session_destroy();
+        exit;
+    }
+
+    function doRedirect($location) {
+        // Redirect user to specified path.
+        header("location: " . $location);
+        exit;
+    }
+
+    function setSessionTimestamp() {
+        // Set new time for session
+        $_SESSION["timestamp"] = time();
+    }
+
+    function checkSessionTimestamp($duration) {
+        // Check if the session timestamp has ben set.
+        if( isset($_SESSION['timestamp']) )
+        {
+            // Subtract the session time (current time - time from session) from the allowed duration. 
+            $time = ($duration - (time() - $_SESSION['timestamp']));
+
+            // If the session time is smaller or equal to 0, the user will be logged out.
+            if($time <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // TODO - How to handle no timestamp?
+
+    }
+
+?>
